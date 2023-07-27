b8425fc72e2a69911a020ea07849a130f52d01ab
krunalsukhwani <68172724+krunalsukhwani@users.noreply.github.com>
2023-07-26 21:07:25 -0400

    login and home component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  email: string="";
+  password: string="";
+  isLogin: boolean=true;
+  errorMessage: string="";
+
+  constructor(private router:Router, private http:HttpClient){}
+
+  login(){
+    let bodyData = {
+      email: this.email,
+      password: this.password
+    };
+
+    this.http.post("http://localhost:4000/user/login", bodyData).subscribe((resultData: any)=>{
+      if(resultData.status){
+        this.router.navigateByUrl('/home');
+      }else{
+        alert('Incorrect Email or Password');
+      }
+    });
+  }
 
 }
